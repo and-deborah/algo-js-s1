acb43be7aae604fe087a251badaf7ae2dfbcb4a0
and-deborah <146062273+and-deborah@users.noreply.github.com>
2023-09-27 10:02:58 +0200

    exercise arrays
    first attempt

--- Exercice 6/main.js
+++ Exercice 6/main.js
@@ -1,3 +1,4 @@
+/* numbers = [7, 49, 51, 18, 64]
 for(let i = 0; i < numbers.length; i++){
     numbers[i] += 12
     console.log(numbers)
@@ -10,4 +11,15 @@ bag.push("chargeur") // pour ajouter un élément dans une liste
 console.log(bag)
 
 bag.splice(2,2)  // pour supprimer un élément : on indique de quel index à quel index on veut supprimer (ici, je supprime "bouffe")
-console.log(bag)
+console.log(bag) */
+
+let names = []
+
+names.push("Vincent")
+names.push("Paul")
+names.push("Arthur")
+
+for(let i = 0: i < names.length; i++) {
+    names[i] += "va à la pêche"
+    console.log(i)
+}
