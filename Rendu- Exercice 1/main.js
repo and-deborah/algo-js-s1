c8060c64b5cf895db05fdeb64323e34265f3de5c
and-deborah <146062273+and-deborah@users.noreply.github.com>
2023-09-29 12:39:35 +0200

    adding link source in most of past exercises +full EXERCISE 1 due for sunday

--- Rendu- Exercice 1/main.js
+++ Rendu- Exercice 1/main.js
@@ -1,16 +1,44 @@
-let Character = {
-    name : "John",
-    sanity : 10
+playlist = ["Usseewa - Ado", "Adventure of a Lifetime - Coldplay", "Butter - BTS", "Bohemian Rhapsody - Queen", "Anissa - Wejdene"]  // index 0 --> 4
+
+
+class character {  
+    constructor(name, sanity){
+        this.name = name
+        this.sanity = sanity
+    }   
+
+
+    journey(playlist, trafficLights, taxiSwap, currentMusic) {
+        trafficLights = 0
+        taxiSwap = 0
+        while (trafficLights < 30 && this.sanity > 0){
+            trafficLights += 1
+            currentMusic = playlist[(Math.floor(Math.random() * playlist.length))]
+            console.log(currentMusic + " est jouée à la radio...")
+
+            if (currentMusic === playlist[4]) {
+                this.sanity -= 1
+                taxiSwap += 1
+                if (this.sanity === 0){
+                    console.log(this.name + " n'en peut plus d'entendre Anissa. " + this.name + " finit par exploser au bout de " + trafficLights + " feux.")
+                    break
+                } else{
+                    console.log(this.name + " n'apprécie pas " + currentMusic +" , il change de taxi...")
+                }
+            } else{
+                console.log(" Phew, pour une fois que ce n'est pas Anissa... il semble que " + this.name + " pourra rester dans ce taxi jusqu'au prochain feu...")
+            }
+       
+            
+            if (trafficLights === 30){
+                console.log(this.name + " est bien arrivé à la maison après avoir changé de taxi " + taxiSwap + " fois, GG à lui")
+                break
+            }
+        }
+    }
 }
 
-let Trajet = { // ne faudrait-il pas une classe à la place ?
-    radio : "La radio",
-    lights : 30,
-    swap : 0
-}
 
-musics = ["one", "two", "three", "four", "Anissa - Wejdene"]  // comment on fait de la proba pour une liste... 
+let john = new character("John", 10)
 
-
-// Initialiser un compteur de 30 "Feux Rouges" qui prendra -1 à chaque fois
-// swap aussi est basiquement un compteur
+john.journey(playlist)
