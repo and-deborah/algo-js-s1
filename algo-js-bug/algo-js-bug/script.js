a99785eba5f12195d74fbd6fd36259877a3c87c8
and-deborah <146062273+and-deborah@users.noreply.github.com>
2023-09-29 09:13:38 +0200

    fixed version of algo-js-bug

--- algo-js-bug/algo-js-bug/script.js
+++ algo-js-bug/algo-js-bug/script.js
@@ -2,19 +2,21 @@
 
 
 
-let users = ["Stéphanie" Gaïa", "Etienne", "Michel"; "Roberto", "Julie"]
+let users = ["Stéphanie", "Gaïa", "Etienne", "Michel", "Roberto", "Julie"]  // faute ici
 
-function countCharacter(value) {
+console.log(users)
+
+function countCharacter(value) {  // faute ici
     return value.length
 }
 
-users.forEach(user => {
-    if (countcharacter(users) < 5) {
-        console.log('c'est un prénom long de plus de 5 lettres.')
+users.forEach(user => { 
+    if (countCharacter(users) > 5) {  // faute ici
+        console.log("c'est un prénom long de plus de 5 lettres.")  // faute ici
     } else {
         console.log("ce n'est pas du tout un prénom long.")
     }
-});
+})  // faute ici
 
 
 
