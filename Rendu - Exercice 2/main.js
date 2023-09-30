9b67efb612983f2bc228c59bbc6c74e3cecee53b
and-deborah <146062273+and-deborah@users.noreply.github.com>
2023-09-30 20:39:21 +0200

    adding a few code lines for Exercise 2 due to sunday

--- Rendu - Exercice 2/main.js
+++ Rendu - Exercice 2/main.js
@@ -1,4 +1,4 @@
-nameList = [Nerd, Insomniac, Thinker, Noob, Speedrunner, Cheater, Pro, Crybaby, Tanker, Brute]
+nameList = [Nerd, Insomniac, Thinker, Noob, Speedrunner, Cheater, Pro, Crybaby, Tanker, God]
 deadlyAttack = [0.001, 0.5, 0.8, 0.666, 0.25]
 
 
@@ -17,11 +17,57 @@ class survivor {
         this.atkRate = atkRate
         this.atkButRip = atkButRip
     }
+
+
+    dying(survivorList){
+        if (this.ripRate > Math.random()){
+            survivorList.splice(indexOf(this.name), 1)
+            console.log(this.name + " est mort sous l'attaque de " + killer.name + ".")
+        }
+    }
+
+
+    dodge(){
+        if (this.atkRate > Math.random()){
+            killer.hp -= 10
+            console.log(this.name + " esquive l'assaut de " + killer.name + " et lui inflige 10 dégâts en retour!")
+        }
+    }
+
+
+
+    lastStraw(survivorList){
+        if (this.atkButRip > Math.random()){
+            killer.hp -= 15
+            survivorList.splice(indexOf(this.name), 1)
+            console.log(this.name + " a utilisé ses dernières forces pour infliger 15 points de dégâts à " + killer.name + ".")
+        }
+    }
+
 }
 
 
 
+victimActions = [dying(survivorList), dodge(), lastStraw(survivorList)]
 
+victimActions[(Math.floor(Math.random() * victimActions.length))]
+
+
+
+/* POUR CODER LES ATTAQUES DE JASON :
+FAIRE UN PICK RANDOM SUR L'UN DES SURVIVANTS PRESENTS DANS LA LISTE "survivorList"
+La ligne de code en question :  victim = survivorList[(Math.floor(Math.random() * survivorList.length))]
+
+
+
+while killer.hp > 0 || survivorList != [] {
+    victim = survivorList[(Math.floor(Math.random() * survivorList.length))]
+    console.log(killer.name + " attaque " + victim + "!")
+    victimActions[(Math.floor(Math.random() * victimActions.length))]   # déclenche une attaque contre la victime à l'aide de la liste victimActions 
+    
+}
+
+*/
 
 
 
@@ -42,3 +88,5 @@ let survivorThree = new survivor(nameList[(Math.floor(Math.random() * nameList.l
 let survivorFour = new survivor(nameList[(Math.floor(Math.random() * nameList.length))], 0.9, 0.9, deadlyAttack[(Math.floor(Math.random() * deadlyAttack.length))])
 
 let survivorFive = new survivor(nameList[(Math.floor(Math.random() * nameList.length))], 0.1, 0.3, deadlyAttack[(Math.floor(Math.random() * deadlyAttack.length))])
+
+survivorList = [survivorOne, survivorTwo, survivorThree, survivorFour, survivorFive]
